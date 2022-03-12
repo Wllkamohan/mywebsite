11632e9184b8019225bac5a797561eaee0245d47
hiddenking <thehiddenking0@gmail.com>
2022-03-12 08:40:47 -0800

    commit

--- website/urls.py
+++ website/urls.py
@@ -10,11 +10,4 @@ from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('affiliate.urls')),
-    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
-
-urlpatterns=patterns+static(settings.MEDIA_URL,document_root.settings.MEDIA_ROOTS)
-
-
-
