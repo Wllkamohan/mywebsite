a87d485472a18b1013c39609b11d36d9fbdc73cb
Wllkamohan <101052469+Wllkamohan@users.noreply.github.com>
2022-03-12 20:25:42 -0800

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,13 @@
+import patterns as patterns
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+from django.views.static import serve
+from django.conf.urls import url
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('affiliate.urls')),
+]
